bb51dea613a0bb8b4edc2db465632e6060d9e21c
vaizs <zxcrigas008@gmail.com>
2024-04-21 13:42:05 +0500

    Za

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Azimkin top
+        </h1>
+        <a
+          className="App-link"
+          href="https://mirage-play.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mirage the best RPG server
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
